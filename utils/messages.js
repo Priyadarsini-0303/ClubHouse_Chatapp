86a8df1b43a51119d221349d111945450934635e
Priya darsini Natarajan <84026251+Priyadarsini-0303@users.noreply.github.com>
2024-07-06 23:08:35 +0530

    Add files via upload

---
+++ utils/messages.js
@@ -0,0 +1,13 @@
+
+// INORDER TO DISPLAY THE USER NAME AND TIME AT THE TOP OF THE MESSAGE 
+const moment = require('moment');
+
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format('h:mm a')
+  };
+}
+
+module.exports = formatMessage;
